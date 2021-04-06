83f077c30e5c099d5fa3c4bd38bf4c48f6c61bd7
Grixi <43199888+Grixi@users.noreply.github.com>
2021-04-06 19:30:26 +0200

    hero page done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,13 @@
-import "../styles/tailwind.css";
-import Navbar from "../components/Navbar";
-import {ThemeProvider} from "next-themes";
-import Footer from "../components/Footer";
+import '@/styles/tailwind.css';
+import Navbar from '@/components/Navbar';
+import { ThemeProvider } from 'next-themes';
 
-
-function MyApp({Component, pageProps}) {
-
-
+function MyApp({ Component, pageProps }) {
   return (
-      <ThemeProvider forcedTheme={Component.theme || undefined} attribute="class">
-        <Navbar/>
-        <Component {...pageProps} />
-        <Footer/>
-      </ThemeProvider>
+    <ThemeProvider attribute="class">
+      <Navbar />
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
 
