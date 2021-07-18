f227b749ba9b354a863a3bb32ee3ba9cce04f231
Grixi <43199888+Grixi@users.noreply.github.com>
2021-07-18 19:49:27 +0200

    done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,9 +13,7 @@ export default function Navbar() {
 
   const navigation = [
     { name: t.home, href: '/' },
-    { name: t.projects_nav, href: '/projects' },
-    { name: t.blog, href: '/blog' },
-    { name: t.about, href: '/about' }
+    { name: t.blog, href: '/blog' }
   ];
   return (
     <Disclosure
--- components/ProjectsCards/CardData.ts
+++ components/ProjectsCards/CardData.ts
@@ -32,7 +32,7 @@ export default [
   },
   {
     name: 'Fakturi3',
-    id: 6,
+    id: 7,
     image: '/images/projects/gpfc.png',
     github: 'sasd'
   }
