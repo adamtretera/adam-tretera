44f635a999afc88c564a4c8e662c8b74d97ed9f0
Grixi <43199888+Grixi@users.noreply.github.com>
2021-07-18 19:49:53 +0200

    done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,6 +13,7 @@ export default function Navbar() {
 
   const navigation = [
     { name: t.home, href: '/' },
+    { name: t.projects_nav, href: '/projects' },
     { name: t.blog, href: '/blog' }
   ];
   return (
