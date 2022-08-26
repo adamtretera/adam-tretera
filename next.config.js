8c18fed357d09847428ae7139d127ba56ff69ec7
Adam Tretera <adam.tretera@ackee.cz>
2022-08-26 13:56:55 +0200

    🔒️  Added security headers

--- next.config.js
+++ next.config.js
@@ -1,15 +1,74 @@
-
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 });
 module.exports = withBundleAnalyzer({
-
-i18n: {
-    locales: ['en', 'cs'],
-    defaultLocale: 'cs'
-  },
-  images: {
-    domains: ['www.notion.so']
-  }
+    async headers() {
+        return [
+            {
+                source: '/',
+                headers: securityHeaders,
+            },
+            {
+                source: '/:path*',
+                headers: securityHeaders,
+            },
+        ];
+    },
+    i18n: {
+        locales: ['en', 'cs'],
+        defaultLocale: 'cs',
+    },
+    images: {
+        domains: ['www.notion.so'],
+    },
 });
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  child-src *.youtube.com *.google.com *.twitter.com;
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  media-src 'none';
+  frame-ancestors self;
+  connect-src *;
+  font-src 'self';
+`;
+
+const securityHeaders = [
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+    {
+        key: 'Content-Security-Policy',
+        value: ContentSecurityPolicy.replace(/\n/g, ''),
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+    // {
+    //     key: 'Referrer-Policy',
+    //     value: 'origin-when-cross-origin'
+    // },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+    {
+        key: 'X-Frame-Options',
+        value: 'DENY',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+    {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+    {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+    {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=31536000; includeSubDomains; preload',
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+    {
+        key: 'Permissions-Policy',
+        value: 'camera=(), microphone=(), geolocation=()',
+    },
+];
